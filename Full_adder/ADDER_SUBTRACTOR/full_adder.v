57551467f74b061388a7c69d60bc032dffb383f3
Amrutha M G <amruthamg27102003@gmail.com>
2025-11-01 22:55:10 +0530

    Delete Full_adder directory

--- Full_adder/ADDER_SUBTRACTOR/full_adder.v
+++
@@ -1,9 +0,0 @@
-module fadder (a,b,cin,sum,carry);
-  input a,b;
-  input cin;
-  output carry;
-  output sum;
-    assign sum = a ^ b ^ cin;
-    assign carry = (a&b) | (b&cin) | ( a&cin);
-  //assign {carry,sum} = a+b+cin;
-endmodule
